b0b1176169a29102c987a07529e8607e634484bc
Felix Schlitter <felixschlitter@gmail.com>
2021-02-26 09:11:28 +1300

    Update purec record API usage

--- src/Record/Builder.c
+++ src/Record/Builder.c
@@ -12,9 +12,9 @@ PURS_FFI_FUNC_3(Record_Builder_unsafeInsert, _key, value, _record) {
 	const purs_str_t *key  = purs_any_force_string(_key);
 	if (record == NULL) {
 		PURS_RC_RELEASE(key);
-		return purs_any_record(purs_record_new_va(1, key->data, value));
+		return purs_any_record(purs_record_new_va(1, key, value));
 	}
-	purs_record_add_mut(record, key->data, value);
+	purs_record_add_mut(record, key, value);
 	PURS_RC_RELEASE(key);
 	return _record;
 }
@@ -22,10 +22,10 @@ PURS_FFI_FUNC_3(Record_Builder_unsafeInsert, _key, value, _record) {
 PURS_FFI_FUNC_3(Record_Builder_unsafeModify, _key, f, _record) {
 	const purs_str_t *key = purs_any_force_string(_key);
 	purs_record_t *record = (purs_record_t *) purs_any_force_record(_record);
-	const purs_any_t *hit = purs_record_find_by_key(record, key->data);
+	const purs_any_t *hit = purs_record_find_by_key(record, key);
 	purs_assert(hit != NULL, "missing key");
 	purs_any_t tmp1 = purs_any_app(f, *hit);
-	purs_record_add_multi_mut(record, 1, key->data, tmp1);
+	purs_record_add_multi_mut(record, 1, key, tmp1);
 	PURS_ANY_RELEASE(tmp1);
 	PURS_RC_RELEASE(key);
 	return _record;
@@ -34,7 +34,7 @@ PURS_FFI_FUNC_3(Record_Builder_unsafeModify, _key, f, _record) {
 PURS_FFI_FUNC_2(Record_Builder_unsafeDelete, _key, _record) {
 	const purs_str_t *key = purs_any_force_string(_key);
 	purs_record_t *record = (purs_record_t*) purs_any_force_record(_record);
-	purs_record_remove_mut(record, key->data);
+	purs_record_remove_mut(record, key);
 	PURS_RC_RELEASE(key);
 	return _record;
 }
@@ -43,10 +43,10 @@ PURS_FFI_FUNC_3(Record_Builder_unsafeRename, _keyOld, _keyNew, _record) {
 	const purs_str_t *key_old = purs_any_force_string(_keyOld);
 	const purs_str_t *key_new = purs_any_force_string(_keyNew);
 	purs_record_t *record = (purs_record_t*)purs_any_force_record(_record);
-	const purs_any_t *hit = purs_record_find_by_key(record, key_old->data);
+	const purs_any_t *hit = purs_record_find_by_key(record, key_old);
 	assert(hit != NULL);
-	purs_record_add_multi_mut((purs_record_t*) record, 1, key_new->data, *hit);
-	purs_record_remove_mut((purs_record_t*) record, key_old->data);
+	purs_record_add_multi_mut((purs_record_t*) record, 1, key_new, *hit);
+	purs_record_remove_mut((purs_record_t*) record, key_old);
 	PURS_RC_RELEASE(key_old);
 	PURS_RC_RELEASE(key_new);
 	return _record;
--- src/Record/ST.c
+++ src/Record/ST.c
@@ -15,7 +15,7 @@ PURS_FFI_FUNC_2(Record_ST_thaw, _record, _) {
 PURS_FFI_FUNC_3(Record_ST_unsafePeek, _key, _record, _) {
 	const purs_str_t *key = purs_any_force_string(_key);
 	const purs_record_t *record = purs_any_unsafe_get_record(_record);
-	purs_any_t *hit = purs_record_find_by_key(record, key->data);
+	purs_any_t *hit = purs_record_find_by_key(record, key);
 	assert(hit != NULL);
 	PURS_ANY_RETAIN(*hit);
 	PURS_RC_RELEASE(key);
@@ -25,7 +25,7 @@ PURS_FFI_FUNC_3(Record_ST_unsafePeek, _key, _record, _) {
 PURS_FFI_FUNC_4(Record_ST_unsafePoke, _key, value, _record, _) {
 	const purs_str_t *key = purs_any_force_string(_key);
 	purs_record_t *record = (purs_record_t*)purs_any_unsafe_get_record(_record);
-	purs_record_add_mut(record, key->data, value);
+	purs_record_add_mut(record, key, value);
 	PURS_RC_RELEASE(key);
 	return purs_any_null;
 }
@@ -33,10 +33,10 @@ PURS_FFI_FUNC_4(Record_ST_unsafePoke, _key, value, _record, _) {
 PURS_FFI_FUNC_4(Record_ST_unsafeModify, _key, f, _record, _) {
 	const purs_str_t *key = purs_any_force_string(_key);
 	purs_record_t *record = (purs_record_t*)purs_any_unsafe_get_record(_record);
-	purs_any_t *hit = purs_record_find_by_key(record, key->data);
+	purs_any_t *hit = purs_record_find_by_key(record, key);
 	assert(hit != NULL);
 	purs_any_t tmp1 = purs_any_app(f, *hit);
-	purs_record_add_mut(record, key->data, tmp1);
+	purs_record_add_mut(record, key, tmp1);
 	PURS_ANY_RELEASE(tmp1);
 	PURS_RC_RELEASE(key);
 	return purs_any_null;
